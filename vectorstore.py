ec1463716db80da54872fb60ae5b89c3c5f6f65d
Roohitkathiresan <110052643+Roohitkathiresan@users.noreply.github.com>
2025-09-22 23:27:34 +0530

    Update vectorstore.py

--- vectorstore.py
+++ vectorstore.py
@@ -1,22 +1,24 @@
 from sentence_transformers import SentenceTransformer
 import chromadb
-from chromadb.utils import embedding_functions
-client = chromadb.Client()
-collection = client.get_or_create_collection("edugenie_docs")
-embed_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-def add_document(doc_id, text):
-    embedding = embed_model.encode(text).tolist()
+client = chromadb.Client()
+collection = client.create_collection("ncert_chapters")
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def add_chapter_to_vectorstore(chapter_name, text):
+    embedding = model.encode(text).tolist()
     collection.add(
         documents=[text],
-        embeddings=[embedding],
-        ids=[doc_id]
+        metadatas={"chapter": chapter_name},
+        ids=[chapter_name],
+        embeddings=[embedding]
     )
-
-def search(query, top_k=3):
-    query_embedding = embed_model.encode(query).tolist()
+def search_vectorstore(query, top_k=3):
+    query_embedding = model.encode(query).tolist()
     results = collection.query(
         query_embeddings=[query_embedding],
-        n_results=top_k
+        n_results=top_k,
+        include=['documents']
     )
-    return results['documents'][0] if results['documents'] else []
+    documents = results['documents'][0]  
+    return documents
