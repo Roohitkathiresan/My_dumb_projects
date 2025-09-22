6f0ec25b9c815aa4dd8e7e33ed4deb1ebd81a241
Roohitkathiresan <110052643+Roohitkathiresan@users.noreply.github.com>
2025-09-22 23:26:39 +0530

    Update ragpipeline.py

--- ragpipeline.py
+++ ragpipeline.py
@@ -1,16 +1,11 @@
-from resources import fetch_wikipedia, fetch_ncert
-from vectorstore import search, add_document
+from resources import fetch_ncert_online
+from vectorstore import search_vectorstore
 
-def build_context(query):
-    context_parts = []
-    docs = search(query)
-    if docs:
-        context_parts.append("VectorStore: " + " ".join(docs))
-    ncert_info = fetch_ncert(query)
-    if ncert_info:
-        context_parts.append("NCERT: " + ncert_info)
-    wiki_info = fetch_wikipedia(query)
-    if wiki_info:
-        context_parts.append("Wikipedia: " + wiki_info)
-
-    return "\n".join(context_parts)
+def build_context(topic):
+    subject = "physics"
+    class_num = 9
+    chapter_name = "motion"  
+    ncert_text = fetch_ncert_online(subject, class_num, chapter_name)
+    vs_context = search_vectorstore(topic)
+    context = ncert_text + "\n\n" + "\n\n".join(vs_context)
+    return context
